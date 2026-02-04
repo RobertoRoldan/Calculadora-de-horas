39fd3b8409ffdb6efdfaf05835263f9b4ebb0396
RobertoRoldan <roberto.roldan.betiko@gmail.com>
2026-02-04 11:20:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE_NAME = 'timecalc-v1';
+const CACHE_NAME = 'timecalc-v3';
 const ASSETS = [
   './',
   './index.html',
-  'https://cdn-icons-png.flaticon.com/512/2082/2082582.png'
+  './manifest.json'
 ];
 
-// Instalar y cachear recursos
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Estrategia: Primero Cache, luego Red
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
